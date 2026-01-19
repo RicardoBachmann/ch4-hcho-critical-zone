fbc4e17fc229c8264c87604ef6ef8e281688e206
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2026-01-19 11:12:02 +0100

    Mapbox project setup with environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useRef, useState, useEffect } from "react";
 import "./App.css";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
+  // Map instance
+  const mapRef = useRef();
+  // Map container html-el
+  const mapContainerRef = useRef();
+  const accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
+
+  useEffect(() => {
+    mapboxgl.accessToken = accessToken;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+    return () => {
+      mapRef.current.remove();
+    };
+  });
+
   return (
     <>
       <h1>Critical Zone</h1>
       <h3>geo-social conflicts</h3>
+      <div id="map-container" ref={mapContainerRef} />
     </>
   );
 }
