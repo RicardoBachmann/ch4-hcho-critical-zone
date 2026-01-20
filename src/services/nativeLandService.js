765a1d9ac740931f42d1d437d2d164ed3571c0be
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2026-01-20 14:27:58 +0100

    fix: File naming to proper camelCase

--- src/services/nativeLandService.js
+++ src/services/nativeLandService.js
@@ -1,26 +1,22 @@
 async function fetchNativeLandService() {
-    console.log("===NATIVE LAND-API SERVICE===")
+  console.log("===NATIVE LAND-API SERVICE===");
 
+  const nativeLandApiKey = import.meta.env.VITE_NATIVE_LAND_API_KEY;
+  const url = `https://native-land.ca/api/polygons/geojson/languages?key=${nativeLandApiKey}`;
 
-    const nativeLandApiKey = import.meta.env.VITE_NATIVE_LAND_API_KEY;
-    const url = `https://native-land.ca/api/polygons/geojson/languages?key=${nativeLandApiKey}`
-
-
-    try {
-        const response = await fetch(url)
-        if(!response.ok) {
-            throw new Error (`Native Land-API Error:, ${response.status}`)
-        }
-        const data = await response.json()
-        console.log("Raw data:", data)
-        return data
-
-    } catch (error) {
-        console.error("Native Land API Error:", error)
-        throw new Error (`Failed to fetch Native Land data: ${error.message}`)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Native Land-API Error: ${response.status}`);
     }
 
-
+    const data = await response.json();
+    console.log("Raw data:", data);
+    return data;
+  } catch (error) {
+    console.error("Native Land API Error:", error);
+    throw new Error(`Failed to fetch Native Land data: ${error.message}`);
+  }
 }
 
 export default fetchNativeLandService;
