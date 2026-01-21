bf218d9cb8807a6774961c1dc13c04169d84d437
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2026-01-21 08:54:21 +0100

    feat: Dynamic API endpoints for productKey parameters - Add API_COLLECTION object for territories and languages endpoints - Implement devensiv coding with collectionId validation - Add loud fail error handling with descriptive messages - Used Object.keys() for dynamic valid options listing * Next step: Display both API endpoints as data layer on map

--- src/services/nativeLandService.js
+++ src/services/nativeLandService.js
@@ -1,8 +1,22 @@
-async function fetchNativeLandService() {
+async function fetchNativeLandService(productKey) {
   console.log("===NATIVE LAND-API SERVICE===");
-
   const nativeLandApiKey = import.meta.env.VITE_NATIVE_LAND_API_KEY;
-  const url = `https://native-land.ca/api/polygons/geojson/languages?key=${nativeLandApiKey}`;
+
+  // API_COLLECTION object for storing and categorize different Native Land products
+  const API_COLLECTION = {
+    Territories: "territories",
+    Languages: "languages",
+  };
+
+  const collectionId = API_COLLECTION[productKey];
+  // const url = `https://native-land.ca/api/polygons/geojson/languages?key=${nativeLandApiKey}`;
+  const url = `https://native-land.ca/api/polygons/geojson/${collectionId}?key=${nativeLandApiKey}`;
+
+  if (!collectionId) {
+    throw new Error(
+      `Invalid product key: ${productKey}, Valid options are: ${Object.keys(API_COLLECTION).join(", ")}`
+    );
+  }
 
   try {
     const response = await fetch(url);
